5a25a27b5b0dd6e2b36a3e7f4925457570143a8e
Torge Stehr <torge.stehr@gmail.com>
2025-05-08 17:11:55 +0200

    adding favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://torgestehr.com',
     i18n: {
         locales: ['en', 'de'],
         defaultLocale: 'en'
